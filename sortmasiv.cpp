fd5bcab6de162106d5effd157cc9cfc641efd2d2
dzisinho <dzisbogdan26@gmail.com>
2024-12-18 20:08:20 +0200

    completed

--- sortmasiv.cpp
+++ sortmasiv.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;	
+
+void insertionSort(int arr[], int n) {
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+int main() {
+	int arr[] = { 12, 11, 13, 5, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Before sorting:\n ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+	insertionSort(arr, n);
+	cout << "After sorting:\n ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+	return 0;
+}
